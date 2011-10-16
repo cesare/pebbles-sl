c2e1b4099d018c1166ca4aa96e06449bc3c4896c
SAWADA Tadashi <cesare@mayverse.jp>
2011-10-17 01:04:52 +0900

    fixed all variables and functions to be static.

--- ext/pebbles/sl.c
+++ ext/pebbles/sl.c
@@ -32,11 +32,11 @@
 #include <unistd.h>
 #include "sl.h"
 
-int ACCIDENT  = 0;
-int LOGO      = 0;
-int FLY       = 0;
+static int ACCIDENT  = 0;
+static int LOGO      = 0;
+static int FLY       = 0;
 
-int my_mvaddstr(int y, int x, char *str)
+static int my_mvaddstr(int y, int x, char *str)
 {
     for ( ; x < 0; ++x, ++str)
 	if (*str == '\0')  return ERR;
@@ -45,7 +45,7 @@ int my_mvaddstr(int y, int x, char *str)
     return OK;
 }
 
-void option(char *str)
+static void option(char *str)
 {
     extern int ACCIDENT, FLY, LONG;
 
@@ -59,7 +59,7 @@ void option(char *str)
     }
 }
 
-void main(int argc, char *argv[])
+static void main(int argc, char *argv[])
 {
     int x, i;
 
@@ -88,7 +88,7 @@ void main(int argc, char *argv[])
 }
 
 
-int add_sl(int x)
+static int add_sl(int x)
 {
     static char *sl[LOGOPATTERNS][LOGOHIGHT + 1]
 	= {{LOGO1, LOGO2, LOGO3, LOGO4, LWHL11, LWHL12, DELLN},
@@ -129,7 +129,7 @@ int add_sl(int x)
 }
 
 
-add_D51(int x)
+static add_D51(int x)
 {
     static char *d51[D51PATTERNS][D51HIGHT + 1]
 	= {{D51STR1, D51STR2, D51STR3, D51STR4, D51STR5, D51STR6, D51STR7,
@@ -170,7 +170,7 @@ add_D51(int x)
 }
 
 
-int add_man(int y, int x)
+static int add_man(int y, int x)
 {
     static char *man[2][2] = {{"", "(O)"}, {"Help!", "\\O/"}};
     int i;
@@ -181,7 +181,7 @@ int add_man(int y, int x)
 }
 
 
-int add_smoke(int y, int x)
+static int add_smoke(int y, int x)
 #define SMOKEPTNS	16
 {
     static struct smokes {
